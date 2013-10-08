74f8aa5d97a8d5191fca71e75f8f2360f58624a4
Quentin Perales <perales.quentin@eisti.fr>
2013-10-08 11:49:27 +0200

    ajout fichiers

---
+++ src/convolution.c
@@ -0,0 +1,17 @@
+/*! \file convolution.c
+ *  \author Perales Quentin <peralesque@eisti.eu>
+ *  \version 0.1
+ *  \date  Tue 08 Oct 2013
+ *
+ *  \brief realise la convolution de l image a partir d'un fichier source
+ *
+ *
+ */
+#include "convolution.h"
+
+int convultion (char* input, char* ouput, char* fichierMatrice)
+{
+	printf("Appel de la fonction convolution\n");
+	return (0);
+}
+
---
+++ src/histogramme.c
@@ -0,0 +1,18 @@
+/*! \file histogramme.c
+ *  \author Perales Quentin <peralesque@eisti.eu>
+ *  \version 0.1
+ *  \date  Tue 08 Oct 2013
+ *
+ *  \brief permet de réaliser l'histogramme d'une image en teinte de gris
+ *
+ *
+ */
+
+#include "histogramme.h"
+
+
+int histogramme (char* input, char* ouput)
+{
+	printf("Appel de la fonction histogram\n");
+	return (0);
+}
