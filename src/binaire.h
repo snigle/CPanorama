7271ddd54d869bb45ae7a7550a08f1e49ccf3dc3
Elie Poussou <elie.poussou@eisti.fr>
2013-10-16 13:46:59 +0200

    binary

--- src/binaire.h
+++ src/binaire.h
@@ -1,31 +1,33 @@
-/*! \file binaire.h
- *  \author Lamarche Ludovic <lamarchelu@eisti.eu>
- *  \version 0.1
- *  \date Tue 08 Oct 2013
- *
- *  \brief Génère un fichier en binaire à partir d'un ppm
- *
- *
- */
+/*! \file  binaire.h
+	\author Poussou Elie poussoueli@eisti.eu
+	\date Wed 16 Oct 2013
+	
+	\brief Génère un fichier en binaire à partir d'un ppm                                   
+	
+
+*/
+	
+	
+	
  
 #ifndef __BINAIRE_H__
 #define __BINAIRE_H__
 #include "header.h"
 
 /*!
-	\fn int binaire(char* input, char* output, int seuil)
-	\author LAMARCHE Ludovic <lamarchelu@eisti.eu>
-	\date Tue 08 Oct 2013
-	\brief Génère le binaire de l'output à partir du seuil		
-	\param input : (char*) nom de l'input
-	\param output : (char*) nom de l'output
-	\param seuil : (int) seuil de conversion
-	\return 
-		
-	\remarks ras
+       \fn int binaire(char* input, char* output, int seuil)
+       \author Poussou Elie <poussoueli@eisti.eu>
+       \date Wed 16 Oct 2013
+       \brief Description               
+       \param input : (char*) fichier d'entrée  
+       \param output : (char*) ficier de sortie
+       \param seuil : (int) Seuil
+       \return 0 si le traitement a été effectué
+               
+       \remarks 
 
 */
-int binaire(char* input, char* output, char* seuil);
+int binaire(char* input, char* output, int seuil);
 
 #endif /* __BINAIRE_H__ */
 
