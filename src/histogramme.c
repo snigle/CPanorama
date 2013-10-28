2fcaaa9ab78549e88ab0ed108523d2634106b3cb
Elie Poussou <elie.poussou@eisti.fr>
2013-10-28 17:44:06 +0100

    histogramme

--- src/histogramme.c
+++ src/histogramme.c
@@ -1,18 +1,116 @@
-/*! \file histogramme.c
- *  \author Perales Quentin <peralesque@eisti.eu>
- *  \version 0.1
- *  \date  Tue 08 Oct 2013
- *
- *  \brief permet de réaliser l'histogramme d'une image en teinte de gris
- *
- *
- */
+/*! \file  histogramme.c
+	\author Poussou Elie poussoueli@eisti.eu
+	\date Tue 22 Oct 2013
+	
+	\brief Permet d'avoir l'histogramme d'une image en niveaux de gris                                 
+	
 
+*/
+
+/*
+Pour chaque intensité, compter le nombre de pixels pour chaque intensité
+
+tests sur si l'image est en images de gris et si 
+l'image n'est pas nulle
+*/
 #include "histogramme.h"
 
-
-int histogramme (char* input, char* ouput)
+int* remplirTableauHist(Image image, int* bool_erreur)
 {
-	printf("Appel de la fonction histogram\n");
-	return (0);
+	int* TabHisto;
+	int i;
+	int j;
+	int n;
+	n=256;
+	TabHisto = mallocBis(n * sizeof(int));
+	if(testType(image, "P2"))
+	{
+		for(i=0; i<n ; i++)
+		{
+			TabHisto[i] = 0;
+		}
+	
+		for(i = 0 ; i < image.height ; i++)
+		{
+			for (j = 0; j < image.width; j++)
+			{
+				TabHisto[image.teinte[i][j]]++;
+			}
+		}	
+	}
+	else
+		*bool_erreur =1;
+	return TabHisto;
+}
+
+int calculMax(int* tabhist)
+{
+	int max;
+	int i;
+	max=0;
+	
+	for(i=0 ; i<256 ; i++)
+	{
+		if(tabhist[i]>max)
+			max=tabhist[i];
+	}
+	
+	return(max);
+}
+
+int** Tracer(int* tabhist, int taille)
+{
+	int i;
+	int j;
+	int k;
+	int max;
+	int** trace;
+
+	max=calculMax(tabhist)/500;
+	trace=initMatrice(taille,max);
+	
+	for (i = 0; i < taille; i += 1)
+	{
+		for (j = 0; j < max; j += 1)
+		{
+			trace[j][i]=0;
+		}
+	}
+	for(i=0 ; i<taille ; i++)
+	{
+		for(j=0; j<max ; j++)
+		{
+			if(j==tabhist[i])
+			{
+				for (k = max-1; k >= 0; k -= 1)
+				{
+					trace[k][i]=1;
+				}
+				
+			}
+		}
+	}
+	return(trace);
+}
+
+Image histogramme (char* input, char* output, int bool_save, int* bool_erreur)
+{
+	Image image;
+	Image sortie;
+	int* hist;
+	int** trace;
+	int max;
+	image = chargerImage(input, bool_erreur);
+	
+	if(!*bool_erreur) hist=remplirTableauHist(image, bool_erreur);
+	max=calculMax(hist);
+	
+	trace=Tracer(hist, 256);
+	
+	sortie = creationImage("P1",256,max/500, 0, trace);
+	save(sortie,output,bool_erreur);
+
+	libererImage(image);
+	printf("Appel de la fonction histogramme\n");
+	return sortie;
 }
--- src/histogramme.h
+++ src/histogramme.h
@@ -2,20 +2,61 @@
 #define __HISTOGRAMME_H__
 
 #include "header.h"
+#include "gestionFichier.h"
+
 
 /*!
        \fn int histogramme(char* input, char* output)
        \author PERALES Quentin <peralesque@eisti.eu>
        \date Tue 08 Oct 2013
        \brief réalise l'histogramme dans un fichier .pbm               
-       \param input : (char*) 
-       \param output : (char*) 
+       \param input : (char*) : le nom de l'image en entrée
+       \param output : (char*) : le nom de l'image en sortie
        \return un entier qui confirme le bon fonctionnement de la fonction
                
        \remarks 
 
 */
-int histogramme (char* input, char* ouput);
+Image histogramme (char* input, char* output, int bool_save, int* bool_erreur);
 
+/*!
+       \fn int* remplirTableauHist(Image image)
+       \author Poussou Elie <poussoueli@eisti.eu>
+       \date Mon 28 Oct 2013
+       \brief rempli un tableau de 1 à 256 avec le nombre de pixels pour chaque teinte               
+       \param image : (image) : l'image avec laqulle on travaille
+       \return un tableau
+               
+       \remarks 
+
+*/
+int* remplirTableauHist(Image image, int* bool_erreur);
+
+/*!
+       \fn int calculMax(int* tabhist)
+       \author Poussou Elie <poussoueli@eisti.eu>
+       \date Mon 28 Oct 2013
+       \brief retourne le maximum de tabhist cad le nombre de pixels maximum pour une teinte               
+       \param tabhist : (int*) 
+       \return un entier
+               
+       \remarks 
+
+*/
+int calculMax(int* tabhist);
+
+/*!
+       \fn int** Tracer(Image image, int taille)
+       \author Poussou Elie <poussoueli@eisti.eu>
+       \date Mon 28 Oct 2013
+       \brief permet de tracer l'histogramme               
+       \param image : (image) : l'image dont on veut obtenir l'histogramme
+       \param taille : (int) : largeur de la matrice
+       \return 
+               
+       \remarks 
+
+*/
+int** Tracer(int* tabhist, int taille);
 #endif /* __HISTOGRAMME_H__ */
 
