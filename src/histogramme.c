ba6df2a5b4ce4e9760271d1e3b6cb9ffc649f87a
Quentin Perales <perales.quentin@eisti.fr>
2013-11-20 12:13:13 +0100

    histogramme

--- src/histogramme.c
+++ src/histogramme.c
@@ -102,7 +102,8 @@ Image histogrammeBis(char* str_input, char* str_output, int bool_save, int* bool
 	percent = tabPourcent(hist, max, 500, image.teinteMax);
 	trace = Tracer(percent, 500, image.teinteMax);
 	sortie = creationImage("P1", image.teinteMax, 500, 0, trace);
-	save(sortie, str_output, bool_erreur);
+	if (bool_save)
+		save(sortie, str_output, bool_erreur);
 	printf("\t L'histogramme de l'image %s a été effectuée avec succés. Il est tracé dans le fichier %s \n", str_input, str_output);
 	return (sortie);
 }
